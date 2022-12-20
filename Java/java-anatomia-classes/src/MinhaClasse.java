82310304eeac8e7e9cd181676a5d082f2d38254e
thiagohenriquedev <thiagohenriquedev@gmail.com>
2022-12-19 23:09:28 -0300

    classes

---
+++ Java/java-anatomia-classes/src/MinhaClasse.java
@@ -0,0 +1,12 @@
+public class MinhaClasse {
+    public static void main(String [] args) {
+        String primeiroNome = "Thiago";
+        String segundoNome = "Henrique";
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+        System.out.println(nomeCompleto);
+    } 
+
+    public static String nomeCompleto (String primeiroNome, String segundoNome) {
+        return "Resultado do método " + primeiroNome.concat(" ").concat(segundoNome);
+    }
+}
